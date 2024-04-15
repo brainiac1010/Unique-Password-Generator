f2e91b13fb362b81d4febdc9f136e923a07c2dc3
brainiac1010 <armanmahim00@gmail.com>
2024-04-15 17:51:50 +0600

    some js error fix

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 const passwordBox = document.getElementById("password");
-const length = 12;
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const number = "0123456789";
 const symbol = "!@#$%^&*(_+{}[-=/.,~|?])";
 const allChars = upperCase + lowerCase + number + symbol;
 
-function createPassword() {
-    let password = "";
-    password += upperCase[Math.floor(Math.random() * upperCase.length)];
-    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-    password += number[Math.floor(Math.random() * number.length)];
-    password += symbol[Math.floor(Math.random() * symbol.length)];
-
-    while (length > password.length) {
-        password += allChars[Math.floor(Math.random() * allChars.length)];
-    }
+function generatePassword() {
+    const length = 12; // You can adjust the length here if needed
+    const password = createPassword(length);
     passwordBox.value = password;
+    passwordBox.select();
+    document.execCommand("copy");
 }
 
-function coppyPassword() {
+function createPassword(length) {
+    let password = '';
+    const characters = [upperCase, lowerCase, number, symbol];
+
+    characters.forEach(charSet => {
+        password += charSet[Math.floor(Math.random() * charSet.length)];
+    });
+
+    for (let i = password.length; i < length; i++) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+
+    return password;
+}
+
+function copyPassword() {
     passwordBox.select();
     document.execCommand("copy");
 }
